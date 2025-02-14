ae2a56b9df3069283382fba046ecd83b33006b66
Osmar <osmargm1202@gmail.com>
2025-02-14 08:39:25 -0400

    update enable

--- orgm/cli.py
+++ orgm/cli.py
@@ -1,9 +1,7 @@
 import click
-import colorama
 import json
 import os
 import subprocess
-from colorama import Fore, Style
 from .server.auth import login
 from dotenv import load_dotenv
 
@@ -11,9 +9,6 @@ load_dotenv(override=True)
 
 
 url = os.getenv("SERVER_URL_U")
-
-# Inicializar colorama
-colorama.init(autoreset=True)
 
 # Variable global para almacenar el token
 token = None
@@ -25,20 +20,13 @@ token = None
 @click.help_option("-h", "--help", help="Muestra la ayuda del comando")
 def cli(ctx):
     """Bienvenido a CLI de ORGM"""
-    click.echo(Style.BRIGHT + Fore.CYAN + "Bienvenido a CLI de ORGM")
-    click.echo(Fore.BLUE + "----------------------------------")
+    click.echo("Bienvenido a CLI de ORGM")
+    click.echo("----------------------------------")
     if ctx.invoked_subcommand is None:
         with open("orgm/cache/comandos.json", "r") as f:
             comandos = json.load(f)
         for comando, info in comandos.items():
-            click.echo(
-                Style.BRIGHT
-                + Fore.YELLOW
-                + f"{comando} - "
-                + Style.BRIGHT
-                + Fore.BLUE
-                + info
-            )
+            click.echo(f"{comando} - {info}")
 
             # for key, value in info.items():
             #     click.echo(Fore.BLACK + f"{key}: {value}")
@@ -49,7 +37,7 @@ def cli(ctx):
 @click.argument("edit", nargs=1, required=False)
 def update(edit):
     """Actualizar el paquete de ORGM CLI"""
-    click.echo(Fore.GREEN + "Actualizando paquete de ORGM CLI")
+    click.echo("Actualizando paquete de ORGM CLI")
 
     if edit == "-e":
         try:
@@ -57,28 +45,31 @@ def update(edit):
             subprocess.check_call(
                 [
                     "uv",
-                    "pip",
+                    "tool",
                     "install -e",
-                    "git+ssh://git@github.com/osmargm1202/orgmcli.git",
+                    "git+https://github.com/osmargm1202/cli.git",
                 ]
             )
-            click.echo(Fore.GREEN + "Paquete instalado correctamente.")
+            click.echo("Paquete instalado correctamente.")
         except subprocess.CalledProcessError as e:
-            click.echo(Fore.RED + f"Error al instalar el paquete: {e}")
+            click.echo(f"Error al instalar el paquete: {e}")
     else:
         try:
-            # Ejecuta el comando pip install
             subprocess.check_call(
-                ["pip", "install", "git+ssh://git@github.com/osmargm1202/orgmcli.git"]
+                [
+                    "uv",
+                    "tool",
+                    "install",
+                    "git+https://github.com/osmargm1202/cli.git",
+                ]
             )
-            click.echo(Fore.GREEN + "Paquete instalado correctamente.")
+            click.echo("Paquete instalado correctamente.")
         except subprocess.CalledProcessError as e:
-            click.echo(Fore.RED + f"Error al instalar el paquete: {e}")
+            click.echo(f"Error al instalar el paquete: {e}")
 
 
 cli.add_command(login)
 cli.add_command(update)
-
 
 if __name__ == "__main__":
     cli()
